c6b53e770240e82ebee55be388d9110b58b6ebda
D32\zak <spseplzen@spseplzen.cz>
2025-10-03 12:26:27 +0200

    prvni commit

--- main.py
+++ main.py
@@ -1,2 +1,58 @@
+from math import floor
+from operator import truediv
+
 print("Hello world")
-print("ahoj")
+a = "ahoj"
+b = a
+
+print(b) # komentar
+#komentar2
+# **************
+#dulezity
+# ***********
+
+# datove typy v pythonu
+
+samopopisne_nazvy_promennych = "ahojxoxoxo6767"
+print(samopopisne_nazvy_promennych)
+print(type(samopopisne_nazvy_promennych))
+
+cislo = 9
+print(cislo)
+print(type(cislo))
+
+desetinne_cislo = 5.5
+print(desetinne_cislo)
+print(type(desetinne_cislo))
+
+vysledek = 5 / 7
+print(vysledek)
+print(type(vysledek))
+
+a = "ahoj"
+a = 5
+
+# cisla
+
+a = 5
+b = 7
+c = a + b
+d = a - b
+e = a * b
+f = a / b # nulou se nedeli :) b!=0
+g =  a ** 2
+h = a ** (1/2)
+i = a % b
+
+print(c)
+
+x = 5.5
+print(round(x))
+print(int(x))
+
+y = 5 + 5 * 2
+
+logicka = True
+l2 = False
+
+llll = logicka * l2 or #and je soucin *

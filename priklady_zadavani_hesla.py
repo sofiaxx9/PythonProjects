feb471344f37516f40215e853255a4fa06d9f792
Sofia Duma <sofia.k.duma@gmail.com>
2026-02-06 11:43:14 +0100

    Add files via upload

---
+++ priklady_zadavani_hesla.py
@@ -0,0 +1,6 @@
+heslo=input("zadej heslo: ")
+skutecne_heslo="heslo123"
+if heslo == skutecne_heslo:
+    print("heslo zadano spravne")
+else:
+    print("heslo zadano spatne")
---
+++ uzivatelsky_vstup.py
@@ -0,0 +1,6 @@
+jmeno = input("zadej jmeno:")
+print("uzivatel zadal "+ jmeno)
+
+cislo = input("zadej cislo: ")
+print("uzivatel zadal cislo: "+cislo)
+print(type(cislo))

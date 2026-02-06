334486d3f6ff8c3e83eb47829c8d40da0f024178
Sofia Duma <sofia.k.duma@gmail.com>
2026-02-06 12:27:59 +0100

    Add files via upload

---
+++ guess_hra.py
@@ -0,0 +1,16 @@
+import numpy as np
+for i in range(10):
+    cislo = np.random.randint(10,20)
+    rnd = np.random.random(5)
+    print(cislo)
+    print(rnd)
+
+    #guess the number hra
+    if pokus == cislo:
+    while True:
+        cislo=np.random.randint(0,21)
+        for i in range(21)
+            cislo=input("rekni cislo od 0 do 20")
+            if pokus==cislo:
+                #nedokonceno!
+

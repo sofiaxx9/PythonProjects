9895d16667b9eb356db32c1840c9f97c330049de
Sofia Duma <sofia.k.duma@gmail.com>
2026-02-20 11:55:21 +0100

    Add files via upload

---
+++ faktorial.py
@@ -0,0 +1,6 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * (n-1)
+    print(factorial(5))
--- funkce_fukci.py
+++ funkce_fukci.py
@@ -8,3 +8,13 @@ def vynasob_soucet_a_rozdil(a,b,c,d):
     return soucet * rozdil
 vysledek = vynasob_soucet_a_rozdil(1,2,3,4)
 print("vysledek (a+b) * (c-d) =" + str(vysledek))
+
+def kalkulacka(a,b, funkce):
+    return funkce(a,b)
+vysledek = vynasob_soucet_a_rozdil(1,2,5,3)
+print("vysledek (a+b) * (c-d) ="+ str(vysledek))
+
+vysledek2= kalkulacka(2,3,secti)
+vysledek3= kalkulacka(2,3,odecti)
+print(vysledek2)
+print(vysledek3)
